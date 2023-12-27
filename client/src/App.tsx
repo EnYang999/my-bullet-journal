d6372cf4724058bf8bd515ca5ec3514091d7b379
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-27 16:09:11 -0500

    not that useful change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { SeptemberWeek } from "../src/components/SeptemberWeek/SeptemberWeek";
+import RabbitStamp from "./components/rabbitstamp/rabbitstamp";
+import Monday from "./components/monday/Monday";
 function App() {
 	return (
 		<>
-			<SeptemberWeek className={"temp_1"} />
+			{/* <RabbitStamp stampWidth={10} /> */}
+			{/* <SeptemberWeek className={"temp_1"} /> */}
+			<Monday />
 		</>
 	);
 }
