bf518357fa6c4b02b276d89f766d2b6c3983e158
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-28 18:50:37 -0500

    not important change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,21 @@ import "./App.css";
 import { SeptemberWeek } from "../src/components/SeptemberWeek/SeptemberWeek";
 import RabbitStamp from "./components/rabbitstamp/rabbitstamp";
 import Monday from "./components/monday/Monday";
+import Tuesday from "./components/tuesday/Tuesday";
+import Wednesday from "./components/wednesday/Wednesday";
+import Thursday from "./components/thursday/Thursday";
+import Friday from "./components/friday/Friday";
+import Weekend from "./components/weekend/Weekend";
+import WeekContainer from "./components/week-box-container/WeekContainer";
+import SeptemberWeekMobile from "./components/SeptemberWeekMobile/SeptemberWeekMobile";
+import SidePage from "./components/sidebarpage/SidePage";
 function App() {
 	return (
 		<>
 			{/* <RabbitStamp stampWidth={10} /> */}
 			{/* <SeptemberWeek className={"temp_1"} /> */}
-			<Monday />
+			<SeptemberWeekMobile />
+			{/* <SidePage /> */}
 		</>
 	);
 }
