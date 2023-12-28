cc558974f271ed37dde87d708486c5117891466c
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-28 16:47:50 -0500

    sidebar-html/css

--- client/src/components/sidebarpage/SidePage.tsx
+++ client/src/components/sidebarpage/SidePage.tsx
@@ -6,73 +6,73 @@ const SidePage = () => {
 				<ul>
 					<li className='active'>
 						<a href='#'>
-							<span className='icon'>J</span>
+							<span className='month-cap'>J</span>
 							<span className='month-name'>an</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>F</span>
+							<span className='month-cap'>F</span>
 							<span className='month-name'>eb</span>
 						</a>
 					</li>
 					<li className='active'>
 						<a href='#'>
-							<span className='icon'>M</span>
+							<span className='month-cap'>M</span>
 							<span className='month-name'>ar</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>A</span>
+							<span className='month-cap'>A</span>
 							<span className='month-name'>pr</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>M</span>
+							<span className='month-cap'>M</span>
 							<span className='month-name'>ay</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>J</span>
+							<span className='month-cap'>J</span>
 							<span className='month-name'>un</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>J</span>
+							<span className='month-cap'>J</span>
 							<span className='month-name'>ul</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>A</span>
+							<span className='month-cap'>A</span>
 							<span className='month-name'>ug</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>S</span>
+							<span className='month-cap'>S</span>
 							<span className='month-name'>ep</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>O</span>
+							<span className='month-cap'>O</span>
 							<span className='month-name'>ct</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>N</span>
+							<span className='month-cap'>N</span>
 							<span className='month-name'>ov</span>
 						</a>
 					</li>
 					<li>
 						<a href='#'>
-							<span className='icon'>D</span>
+							<span className='month-cap'>D</span>
 							<span className='month-name'>ec</span>
 						</a>
 					</li>
