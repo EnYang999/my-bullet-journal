7aaf70a8c351d989ada8952d67fb9edfd7857406
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-28 16:19:20 -0500

    sidepage

---
+++ client/src/components/sidebarpage/SidePage.tsx
@@ -0,0 +1,85 @@
+import "./SidePage.css";
+const SidePage = () => {
+	return (
+		<div className='pagination-container'>
+			<div className='pagination-overlap'>
+				<ul>
+					<li className='active'>
+						<a href='#'>
+							<span className='icon'>J</span>
+							<span className='month-name'>an</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>F</span>
+							<span className='month-name'>eb</span>
+						</a>
+					</li>
+					<li className='active'>
+						<a href='#'>
+							<span className='icon'>M</span>
+							<span className='month-name'>ar</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>A</span>
+							<span className='month-name'>pr</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>M</span>
+							<span className='month-name'>ay</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>J</span>
+							<span className='month-name'>un</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>J</span>
+							<span className='month-name'>ul</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>A</span>
+							<span className='month-name'>ug</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>S</span>
+							<span className='month-name'>ep</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>O</span>
+							<span className='month-name'>ct</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>N</span>
+							<span className='month-name'>ov</span>
+						</a>
+					</li>
+					<li>
+						<a href='#'>
+							<span className='icon'>D</span>
+							<span className='month-name'>ec</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default SidePage;
