3aa17e207733c4b45ef1f8edc10538b05ee99917
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-28 18:45:30 -0500

    responsive mobile

--- client/src/components/SeptemberWeekMobile/SeptemberWeekMobile.tsx
+++ client/src/components/SeptemberWeekMobile/SeptemberWeekMobile.tsx
@@ -1,9 +1,11 @@
 import WeekContainer from "../week-box-container/WeekContainer";
 import RabbitStamp from "../rabbitstamp/rabbitstamp";
 import "./SeptemberWeekMobile.css";
+import SidePage from "../sidebarpage/SidePage";
 const SeptemberWeekMobile = () => {
 	return (
 		<div className='september-week-mobile'>
+			{/* <SidePage className='sidebar' /> */}
 			<RabbitStamp stampWidth={10} className='rabbit' />
 			<WeekContainer className='WeekContainer' />
 		</div>
--- client/src/components/sidebarpage/SidePage.tsx
+++ client/src/components/sidebarpage/SidePage.tsx
@@ -1,81 +1,67 @@
+import { useState } from "react";
 import "./SidePage.css";
-const SidePage = () => {
+interface Props {
+	className: string;
+}
+const SidePage = ({ className }: Props): JSX.Element => {
+	const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+	const handleMouseEnter = (index: number) => {
+		setActiveIndex(index);
+	};
+
+	const handleMouseLeave = () => {
+		setActiveIndex(null);
+	};
+
+	const handleFocus = (index: number) => {
+		setActiveIndex(index);
+	};
+
+	const handleBlur = () => {
+		setActiveIndex(null);
+	};
+
+	const months = [
+		{ abbreviation: "Jan", full: "January" },
+		{ abbreviation: "Feb", full: "February" },
+		{ abbreviation: "Mar", full: "March" },
+		{ abbreviation: "Apr", full: "April" },
+		{ abbreviation: "May", full: "May" },
+		{ abbreviation: "Jun", full: "June" },
+		{ abbreviation: "Jul", full: "July" },
+		{ abbreviation: "Aug", full: "August" },
+		{ abbreviation: "Sep", full: "September" },
+		{ abbreviation: "Oct", full: "October" },
+		{ abbreviation: "Nov", full: "November" },
+		{ abbreviation: "Dec", full: "December" },
+	];
+
 	return (
-		<div className='pagination-container'>
+		<div className={`pagination-container ${className}`}>
 			<div className='pagination-overlap'>
 				<ul>
-					<li className='active'>
-						<a href='#'>
-							<span className='month-cap'>J</span>
-							<span className='month-name'>an</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>F</span>
-							<span className='month-name'>eb</span>
-						</a>
-					</li>
-					<li className='active'>
-						<a href='#'>
-							<span className='month-cap'>M</span>
-							<span className='month-name'>ar</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>A</span>
-							<span className='month-name'>pr</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>M</span>
-							<span className='month-name'>ay</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>J</span>
-							<span className='month-name'>un</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>J</span>
-							<span className='month-name'>ul</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>A</span>
-							<span className='month-name'>ug</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>S</span>
-							<span className='month-name'>ep</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>O</span>
-							<span className='month-name'>ct</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>N</span>
-							<span className='month-name'>ov</span>
-						</a>
-					</li>
-					<li>
-						<a href='#'>
-							<span className='month-cap'>D</span>
-							<span className='month-name'>ec</span>
-						</a>
-					</li>
+					{months.map((month, index) => (
+						<li
+							key={index}
+							className={`${
+								index === activeIndex ? "active" : ""
+							} ${month.abbreviation.toLowerCase()}`}
+							onMouseEnter={() => handleMouseEnter(index)}
+							onMouseLeave={handleMouseLeave}
+							onFocus={() => handleFocus(index)}
+							onBlur={handleBlur}
+						>
+							<a href='#'>
+								<span className='month-cap'>
+									{month.abbreviation.charAt(0)}
+								</span>
+								<span className='month-name'>
+									{month.abbreviation.slice(1)}
+								</span>
+							</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
