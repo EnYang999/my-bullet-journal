0db5492a05c38d7732bfd4ded51c7262fa08cbbf
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-12-28 21:36:12 -0500

    september-mobile finished

--- client/src/components/SeptemberWeekMobile/SeptemberWeekMobile.tsx
+++ client/src/components/SeptemberWeekMobile/SeptemberWeekMobile.tsx
@@ -2,7 +2,7 @@ import WeekContainer from "../week-box-container/WeekContainer";
 import RabbitStamp from "../rabbitstamp/rabbitstamp";
 import "./SeptemberWeekMobile.css";
 import RabbitMother from "../RabbitMother/RabbitMother";
-
+import SeptemberLabel from "../septemberlabel/SeptemberLabel";
 const SeptemberWeekMobile = () => {
 	return (
 		<div className='september-week-mobile'>
@@ -10,6 +10,7 @@ const SeptemberWeekMobile = () => {
 			<RabbitStamp stampWidth={10} className='rabbit' />
 			<WeekContainer className='WeekContainer' />
 			<RabbitMother className='rabbitmother' />
+			<SeptemberLabel className='septemberlabel' />
 		</div>
 	);
 };
