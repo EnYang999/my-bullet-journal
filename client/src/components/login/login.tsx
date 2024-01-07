f299de35b3b58c982422741f3a50a6675ed062a0
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-07 01:39:53 -0500

    login checkbox

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,50 +1,9 @@
 import React, { useState } from "react";
 import "./login.css";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-interface FormData {
-	name: string;
-	email: string;
-	password: string;
-}
-
-const pwdValidationMessages = {
-	required: "New Password is required",
-	lowerCase: "One lowercase character",
-	upperCase: "One uppercase character",
-	number: "One number",
-	special: "One special character",
-	min: "Must be at least 8 characters in length",
-};
-const lowerCaseRegex = new RegExp(".*[a-z].*");
-const upperCaseRegex = new RegExp(".*[A-Z].*");
-const numberRegex = new RegExp(".*\\d.*");
-const specialCharacterRegex = new RegExp(
-	".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"
-);
-
-const minLength = 8;
-const userSchema = yup
-	.object({
-		name: yup
-			.string()
-			.required("No name provided.")
-			.min(4, "Name is too short - should be 4 chars minimum."),
-		email: yup
-			.string()
-			.required("No email provided.")
-			.email("Not legally email address."),
-		password: yup
-			.string()
-			.matches(upperCaseRegex, pwdValidationMessages.upperCase)
-			.matches(lowerCaseRegex, pwdValidationMessages.lowerCase)
-			.matches(numberRegex, pwdValidationMessages.number)
-			.matches(specialCharacterRegex, pwdValidationMessages.special)
-			.min(minLength, pwdValidationMessages.min)
-			.required(pwdValidationMessages.required),
-	})
-	.required();
+import Icon from "react-icons-kit";
+import { arrows_exclamation } from "react-icons-kit/linea/arrows_exclamation";
+import { arrows_circle_check } from "react-icons-kit/linea/arrows_circle_check";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Login: React.FC = () => {
 	const [isSignUp, setSignUp] = useState<boolean>(false);
@@ -54,16 +13,44 @@ const Login: React.FC = () => {
 	const handleSignInClick = () => {
 		setSignUp(false);
 	};
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormData>({
-		resolver: yupResolver(userSchema, { abortEarly: false }),
-		criteriaMode: "all",
-		mode: "onChange",
-	});
-	console.log(errors);
+	const [type, setType] = useState("password");
+	const [lowerValidated, setLowerValidated] = useState(false);
+	const [upperValidated, setUpperValidated] = useState(false);
+	const [numberValidated, setNumberValidated] = useState(false);
+	const [specialValidated, setSpecialValidated] = useState(false);
+	const [lengthValidated, setLengthValidated] = useState(false);
+	const handleChange = (value: string) => {
+		const lower = new RegExp("(?=.*[a-z])");
+		const upper = new RegExp("(?=.*[A-Z])");
+		const number = new RegExp("(?=.*[0-9])");
+		const special = new RegExp("(?=.*[!@#$%^&*])");
+		const length = new RegExp("(?=.{8,})");
+		if (lower.test(value)) {
+			setLowerValidated(true);
+		} else {
+			setLowerValidated(false);
+		}
+		if (upper.test(value)) {
+			setUpperValidated(true);
+		} else {
+			setUpperValidated(false);
+		}
+		if (number.test(value)) {
+			setNumberValidated(true);
+		} else {
+			setNumberValidated(false);
+		}
+		if (special.test(value)) {
+			setSpecialValidated(true);
+		} else {
+			setSpecialValidated(false);
+		}
+		if (length.test(value)) {
+			setLengthValidated(true);
+		} else {
+			setLengthValidated(false);
+		}
+	};
 	return (
 		<>
 			<div
@@ -71,14 +58,7 @@ const Login: React.FC = () => {
 				id='container'
 			>
 				<div className='form-container sign-up-container'>
-					<form
-						action='#'
-						className='needs-validation'
-						onSubmit={handleSubmit((data) => {
-							console.log(data);
-						})}
-						noValidate
-					>
+					<form action='#' className='needs-validation' noValidate>
 						{/* <h1>Create Account</h1>
 						<div className='social-container'>
 							<a href='#' className='social'>
@@ -92,9 +72,8 @@ const Login: React.FC = () => {
 							</a>
 						</div>
 						<span>or use your email for registration</span> */}
-						<div className='form-floating mb-2 mt-2'>
+						<div className='form-floating mb-2 mt-2 name-input'>
 							<input
-								{...register("name")}
 								type='text'
 								className='form-control'
 								id='floatingName'
@@ -102,44 +81,98 @@ const Login: React.FC = () => {
 								aria-label='Username'
 							/>
 							<label htmlFor='floatingName'>name</label>
-							{errors.name && (
-								<div className='invalid-feedback'>{errors.name.message}</div>
-							)}
 						</div>
-						<div className='form-floating mb-2'>
+						<div className='form-floating mb-2 email-input'>
 							<input
-								{...register("email")}
 								type='email'
 								className='form-control'
 								id='floatingEmail'
 								placeholder='name@example.com'
 							/>
 							<label htmlFor='floatingEmail'>Email address</label>
-							{errors.email && (
-								<div className='invalid-feedback'>{errors.email.message}</div>
-							)}
 						</div>
 
-						<div className='form-floating  mb-2'>
+						<div className='form-floating  mb-2 password-input'>
 							<input
-								{...register("password")}
-								type='password'
+								type={type}
 								className='form-control'
 								id='floatingPassword'
 								placeholder='Password'
+								onChange={(e) => handleChange(e.target.value)}
 							/>
-							<label htmlFor='floatingPassword'>Password</label>
-							{errors?.password?.types?.required && <p>Password is required</p>}
-							{errors?.password?.types?.minLength && (
-								<p>Password must be at least 10 characters long</p>
-							)}
-							<ul></ul>
+							<span
+								className='icon-span'
+								onClick={() => setType(type === "text" ? "password" : "text")}
+							>
+								<i
+									className={`bi bi-eye${type === "text" ? "-slash" : ""}`}
+								></i>
+							</span>
 
-							{errors.password && (
-								<div className='invalid-feedback'>
-									{errors.password.message}
-								</div>
-							)}
+							<label htmlFor='floatingPassword'>Password</label>
+						</div>
+						{/* validation tracker */}
+						<div className='tracker-box'>
+							<div className={lowerValidated ? "validated" : "not-validated"}>
+								{lowerValidated ? (
+									<span className='list-icon green'>
+										<i className='bi bi-check2-circle'></i>
+									</span>
+								) : (
+									<span className='list-icon'>
+										<i className='bi bi-circle'></i>
+									</span>
+								)}
+								At least one lowercase letter
+							</div>
+							<div className={upperValidated ? "validated" : "not-validated"}>
+								{upperValidated ? (
+									<span className='list-icon green'>
+										<i className='bi bi-check2-circle'></i>
+									</span>
+								) : (
+									<span className='list-icon'>
+										<i className='bi bi-circle'></i>
+									</span>
+								)}
+								At least one uppercase letter
+							</div>
+							<div className={numberValidated ? "validated" : "not-validated"}>
+								{numberValidated ? (
+									<span className='list-icon green'>
+										<i className='bi bi-check2-circle'></i>
+									</span>
+								) : (
+									<span className='list-icon'>
+										<i className='bi bi-circle'></i>
+									</span>
+								)}
+								At least one number
+							</div>
+							<div className={specialValidated ? "validated" : "not-validated"}>
+								{specialValidated ? (
+									<span className='list-icon green'>
+										<i className='bi bi-check2-circle'></i>
+									</span>
+								) : (
+									<span className='list-icon'>
+										<i className='bi bi-circle'></i>
+									</span>
+								)}
+								At least one special character
+							</div>
+							<div className={lengthValidated ? "validated" : "not-validated"}>
+								{lengthValidated ? (
+									<span className='list-icon green'>
+										<i className='bi bi-check2-circle'></i>
+									</span>
+								) : (
+									<span className='list-icon'>
+										<i className='bi bi-circle'></i>
+									</span>
+								)}
+								At least 8 characters
+							</div>
 						</div>
 						<button type='submit'>Sign Up</button>
 					</form>
