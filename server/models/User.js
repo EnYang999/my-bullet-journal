cef5590cb7e53a17869843d86beb9317ea99f8aa
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-10 17:19:44 -0500

    remove the vallidation in schema

--- server/models/User.js
+++ server/models/User.js
@@ -5,23 +5,13 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
-		required: [true, "Please enter an email"],
-		unique: true,
-		lowercase: true,
-		validate: [isEmail, "Please enter a valid email"],
 	},
 	password: {
 		type: String,
-		required: [true, "Please enter a password"],
-		minlength: [6, "Minimum password length is 6 characters"],
 	},
 
 	username: {
 		type: String,
-		required: [true, "Please enter a username"],
-		minlength: [3, "Minimum username length is 3 characters"],
-		unique: false,
-		lowercase: true,
 	},
 });
 
