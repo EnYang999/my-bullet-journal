95453e45a57b826670ed229fa1e7d7a40ed5d135
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-13 15:45:43 -0500

    landing section

---
+++ client/src/components/landingsection/LandingSectiona.tsx
@@ -0,0 +1,54 @@
+import "./LanddingSectiona.scss";
+import img2 from "../../assets/img/2.jpg";
+const LandingSectiona = () => {
+	return (
+		<section className='pt-7 pt-md-9'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-2'>
+						{/* <!--Divider  --> */}
+						<hr className='hr-sm bg-primary' />
+
+						{/* <!--Text  --> */}
+						<p className='fs-lg lh-lg text-black mb-5 mb-md-0'>
+							Take a coffee break or enjoy a meal.
+						</p>
+					</div>
+					<div className='col-md-4'>
+						{/* <!--Media  --> */}
+						<div className='media-decoration media-decoration-1 mb-5 mb-md-0'>
+							{/* <!--Border  --> */}
+							<div
+								className='media-decoration-border'
+								data-jarallax-element='-40'
+							></div>
+
+							{/* <!--Image  --> */}
+							<img
+								className='media-decoration-img img-fluid'
+								src={img2}
+								alt='...'
+							/>
+						</div>
+					</div>
+					<div className='col-md-5 align-self-center mx-auto'>
+						{/* <!--Heading  --> */}
+						<h2 className='mb-2'>
+							<em>Always</em> amazing experience
+						</h2>
+
+						{/* <!--Text  --> */}
+						<p className='mb-0'>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Molestias veritatis nisi, consequatur, laborum libero a neque
+							ducimus. Porro rem illum quo nostrum quisquam asperiores. Possimus
+							facilis velit, voluptatibus!
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default LandingSectiona;
