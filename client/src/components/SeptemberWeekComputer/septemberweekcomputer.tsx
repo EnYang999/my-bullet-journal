da1f3028029e4d5569d1cf364051740250e2128d
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-15 19:25:36 -0500

    structure

--- client/src/components/SeptemberWeekComputer/septemberweekcomputer.tsx
+++ client/src/components/SeptemberWeekComputer/septemberweekcomputer.tsx
@@ -1,19 +1,36 @@
+import SeptemberLabel from "../decorationelements/septemberlabel/SeptemberLabel";
+import SidePage from "../sidebarpage/SidePage";
+import Monday from "./Monday";
+import Tuesday from "./Tuesday";
+import Wednesday from "./Wednesday";
+import Thursday from "./Thursday";
+import Friday from "./Friday";
+import Weekend from "./Weekend";
 import RabbitStamp from "../decorationelements/rabbitstamp/rabbitstamp";
 import "./septemberweekcomputer.css";
 import RabbitMother from "../decorationelements/rabbitmother/RabbitMother";
-import SeptemberLabel from "../decorationelements/septemberlabel/SeptemberLabel";
-import WeekContainerComputer from "./weeb-box-container-computer/WeekContainerComputer";
-import SidePage from "../sidebarpage/SidePage";
-
 const SeptemberWeekComputer = () => {
 	return (
 		<>
 			<div className='september-week-computer'>
 				<div className='content'>
 					<SidePage className='sidebar' />
-					<RabbitStamp stampWidth={10} className='rabbit' />
-					<WeekContainerComputer className='WeekContainer' />
-					<RabbitMother className='rabbitmother' />
+
+					<div className={`week-box-container sep-week-box-computer`}>
+						<div className='week-line week-top'>
+							<Monday />
+							<Tuesday />
+							<Wednesday />
+							<RabbitStamp stampWidth={10} className='rabbit' />
+						</div>
+						<div className='week-line week-bottom'>
+							<RabbitMother className='rabbitmother' />
+							<Thursday />
+							<Friday />
+							<Weekend />
+						</div>
+					</div>
+
 					<SeptemberLabel className='septemberlabel' />
 				</div>
 			</div>
