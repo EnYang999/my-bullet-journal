e3e965501f964ebb885a1ddcda50224c0b8e687d
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-17 17:05:08 -0500

    rabbit mother layout

--- client/src/components/decorationelements/rabbitmother/RabbitMother.tsx
+++ client/src/components/decorationelements/rabbitmother/RabbitMother.tsx
@@ -1,5 +1,5 @@
-import "./RabbitMother.css";
-import star3 from "../../../assets/img/star-vector-3.svg";
+import "./RabbitMother.scss";
+import SeptemberLabel from "../septemberlabel/SeptemberLabel";
 import rabbitMother from "../../../assets/img/rabbit-mother.png";
 interface Props {
 	className: string;
@@ -7,10 +7,9 @@ interface Props {
 const RabbitMother = ({ className }: Props): JSX.Element => {
 	return (
 		<div className={`rabbit-mother-overlap-group ${className}`}>
-			<figure className='figure-rabbitmother-star'>
-				<img className='star-vector-3' alt='Star vector' src={star3} />
+			<figure className='fig-seplabel'>
+				<SeptemberLabel className='septemberlabel' />
 			</figure>
-
 			<figure className='fig-rabbitmother'>
 				<img className='rabbit-mother' alt='Rabbit mother' src={rabbitMother} />
 			</figure>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./components/scss/theme.scss";
 // import "./index.scss";
 // import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
