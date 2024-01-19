dc5e566f399c91de96fcfe4a88f2e1a28f859e7a
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-19 18:54:39 -0500

    side page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
 // import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import SeptemberWeek4 from "./components/SeptemberWeekComputer/septemberweek4";
 import SeptemberWeek1 from "./components/SeptemberWeekComputer/septemberweek1";
 import LandingPage from "./components/landingsection/LandingPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SeptemberWeek3 from "./components/SeptemberWeekComputer/septemberweek3";
 import SeptemberWeek2 from "./components/SeptemberWeekComputer/septemberweek2";
 function App() {
 	return (
 		<>
-			<LandingPage />
+			<Routes>
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/themes/'>
+					<Route index element={<SeptemberWeek1 />} />
+					<Route path='week1' element={<SeptemberWeek1 />} />
+					<Route path='week2' element={<SeptemberWeek2 />} />
+					<Route path='week3' element={<SeptemberWeek3 />} />
+					<Route path='week4' element={<SeptemberWeek4 />} />
+				</Route>
+			</Routes>
+
 			{/* <SeptemberWeek1 /> */}
 			{/* <SeptemberWeek3 /> */}
 		</>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./components/scss/theme.scss";
-// import "./index.scss";
-// import "./index.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap-icons/font/bootstrap-icons.css";
-
 import "flickity/dist/flickity.min.css";
+import SeptemberWeek4 from "./components/SeptemberWeekComputer/septemberweek4";
+import SeptemberWeek1 from "./components/SeptemberWeekComputer/septemberweek1";
+import LandingPage from "./components/landingsection/LandingPage";
+import SeptemberWeek3 from "./components/SeptemberWeekComputer/septemberweek3";
+import SeptemberWeek2 from "./components/SeptemberWeekComputer/septemberweek2";
+
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
