04bb9c1786808f8e6fabbe8517eb68b5050a5c80
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-19 19:00:29 -0500

    delete unnecessary import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./components/scss/theme.scss";
 import "flickity/dist/flickity.min.css";
-import SeptemberWeek4 from "./components/SeptemberWeekComputer/septemberweek4";
-import SeptemberWeek1 from "./components/SeptemberWeekComputer/septemberweek1";
-import LandingPage from "./components/landingsection/LandingPage";
-import SeptemberWeek3 from "./components/SeptemberWeekComputer/septemberweek3";
-import SeptemberWeek2 from "./components/SeptemberWeekComputer/septemberweek2";
-
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
