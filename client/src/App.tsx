fa36edd661e0c45c1a62baddc9f8a5981cd639a3
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-01-24 14:32:19 -0500

    aprilweek4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import SeptemberWeek2 from "./components/SeptemberWeekComputer/septemberweek2";
 import AprilWeek1 from "./components/AprilWeek/aprilweek1";
 import AprilWeek2 from "./components/AprilWeek/aprilweek2";
 import AprilWeek3 from "./components/AprilWeek/aprilweek3";
+import AprilWeek4 from "./components/AprilWeek/aprilweek4";
 import Login from "./components/login/login";
 function App() {
 	return (
@@ -24,7 +25,7 @@ function App() {
 							<Route path='week-1' element={<AprilWeek1 />} />
 							<Route path='week-2' element={<AprilWeek2 />} />
 							<Route path='week-3' element={<AprilWeek3 />} />
-							<Route path='week-4' element={<SeptemberWeek4 />} />
+							<Route path='week-4' element={<AprilWeek4 />} />
 						</Route>
 					</Route>
 				</Route>
---
+++ client/src/components/AprilWeek/weekday4.tsx
@@ -0,0 +1,32 @@
+import { ToDoLine } from "../todolines/todoline";
+interface Props {
+	day: string;
+	shapeImage: string;
+	boxIds: string[];
+}
+const WeekBox = ({ day, shapeImage, boxIds }: Props) => {
+	return (
+		<div
+			className={`week-overlap overlap-${boxIds.length} d-flex flex-column align-items-center`}
+		>
+			<div className='square text-center lh-1' style={{}}>
+				{day}
+			</div>
+			<figure className={`week-fig fig-${day.toLowerCase()} py-4`}>
+				<img
+					className={`week-shape ${day.toLowerCase()}-shape`}
+					src={shapeImage}
+				/>
+			</figure>
+			<div
+				className={`to-do-lines-container mx-3 flex-grow-1 d-flex flex-column justify-content-center`}
+			>
+				{boxIds.map((boxId, index) => (
+					<ToDoLine key={index} className='my-todo-list' boxId={boxId} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default WeekBox;
