f708bdb70fc512801882e480022d741bc33b17ca
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-02-08 18:16:56 -0500

    week4

---
+++ client/src/components/June/week4.tsx
@@ -0,0 +1,102 @@
+import WeekBox from "./weekday4";
+import Calendar from "../calender/Calendar";
+const JuneWeek4 = () => {
+	return (
+		<>
+			<div className='row'>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Monday'
+						boxIds={[
+							`jun-week-2-monday-1`,
+							`jun-week-2-monday-2`,
+							`jun-week-2-monday-3`,
+							`jun-week-2-monday-4`,
+							`jun-week-2-monday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Tuesday'
+						boxIds={[
+							`jun-week-2-tuesday-1`,
+							`jun-week-2-tuesday-2`,
+							`jun-week-2-tuesday-3`,
+							`jun-week-2-tuesday-4`,
+							`jun-week-2-tuesday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Wednesday'
+						boxIds={[
+							`jun-week-2-wednesday-1`,
+							`jun-week-2-wednesday-2`,
+							`jun-week-2-wednesday-3`,
+							`jun-week-2-wednesday-4`,
+							`jun-week-2-wednesday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Thursday'
+						boxIds={[
+							`jun-week-2-thursday-1`,
+							`jun-week-2-thursday-2`,
+							`jun-week-2-thursday-3`,
+							`jun-week-2-thursday-4`,
+							`jun-week-2-thursday-5`,
+						]}
+					/>
+				</div>
+
+				<div className='w-100 d-none d-md-block mt-7'></div>
+
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Friday'
+						boxIds={[
+							`jun-week-2-friday-1`,
+							`jun-week-2-friday-2`,
+							`jun-week-2-friday-3`,
+							`jun-week-2-friday-4`,
+							`jun-week-2-friday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Saturday'
+						boxIds={[
+							`jun-week-2-saturday-1`,
+							`jun-week-2-saturday-2`,
+							`jun-week-2-saturday-3`,
+							`jun-week-2-saturday-4`,
+							`jun-week-2-saturday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<WeekBox
+						day='Sunday'
+						boxIds={[
+							`jun-week-2-sunday-1`,
+							`jun-week-2-sunday-2`,
+							`jun-week-2-sunday-3`,
+							`jun-week-2-sunday-4`,
+							`jun-week-2-sunday-5`,
+						]}
+					/>
+				</div>
+				<div className='col-lg-3'>
+					<Calendar month={6} year={2023} />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default JuneWeek4;
