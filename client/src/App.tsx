e146a00af8eff8268cba8fb2e0305684d3d4e33a
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-05-19 21:04:48 -0400

    delete unnecessary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,7 @@ import LandingPage from "./components/landingsection/LandingPage";
 
 import Login from "./components/login/login";
 import AboutMeHeader from "./components/AboutMe/AboutMeHeader";
-import Temp from "./components/login/template";
-import Tutorial from "./components/login/Tutorial";
+
 import BookTab from "./components/BookTab/BookTab";
 import FullPage from "./components/Full-Page/FullPage";
 import UserProfileWrapper from "./components/UserProfile/UserProfileWrapper";
@@ -14,8 +13,6 @@ function App() {
 		<Routes>
 			<Route path='/' element={<LandingPage />} />
 			<Route path='/login' element={<Login />} />
-			<Route path='/login1' element={<Temp />} />
-			<Route path='/login2' element={<Tutorial />} />
 			<Route path='/about-us' element={<FullPage />} />
 			<Route path='/themes'>
 				<Route index element={<BookTab />} />
--- client/src/components/login/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorPopover = () => {
-	return <div></div>;
-};
-
-export default ErrorPopover;
