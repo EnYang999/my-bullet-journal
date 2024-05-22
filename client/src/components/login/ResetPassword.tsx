e7e64d7a07a7a3e568f316fb9706658af24218f0
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-05-22 15:40:08 -0400

    style simply for reset password

--- client/src/components/login/ResetPassword.tsx
+++ client/src/components/login/ResetPassword.tsx
@@ -1,43 +1,82 @@
 import React, { useState } from "react";
 import axios from "axios";
+import * as constants from "../../../../common/constants";
+import { useNavigate } from "react-router-dom";
+import { toast } from "../errortoast/ErrorToastManager";
 
 const ResetPassword = () => {
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
-
-	const handleSubmit = async (e) => {
+	const navigate = useNavigate();
+	const handleSubmit = async (e: React.SyntheticEvent) => {
 		e.preventDefault();
 		try {
-			const response = await axios.post(
-				"http://localhost:8000/api/users/reset-password",
+			console.log(
+				`${constants.API_ENDPOINT}${constants.APP_USER_API}${constants.APP_RESET_PASSWORD}`
+			);
+
+			const response = await axios.put(
+				`${constants.API_ENDPOINT}${constants.APP_USER_API}${constants.APP_RESET_PASSWORD}`,
 				{ email }
 			);
 			if (response.data.success) {
 				setMessage("Password reset link has been sent to your email address.");
+				toast.show({
+					title: "Success",
+					content: "find the reset link in your email",
+					duration: 15000,
+				});
 			} else {
+				console.log(response.data.message);
+
 				setMessage("Error: " + response.data.message);
+				toast.show({
+					title: "Error",
+					content: "something wrong happened",
+					duration: 15000,
+				});
 			}
+			navigate("/");
 		} catch (error) {
-			setMessage("Error: " + error.message);
+			console.log(error);
+
+			toast.show({
+				title: "Error",
+				content: "You haven't registered",
+				duration: 15000,
+			});
 		}
 	};
 
 	return (
-		<div>
-			<h2>Reset Password</h2>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label>Email:</label>
-					<input
-						type='email'
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
+		<div className='container my-5 py-5'>
+			<div className='row'>
+				<div className='col-md-6 col-sm-12 mx-auto'>
+					<div className='card'>
+						<div className='card-body'></div>
+						<h2 className='card-title'>Reset Password</h2>
+						<form onSubmit={handleSubmit}>
+							<div>
+								<label htmlFor='email'>Email:</label>
+								<input
+									type='email'
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									placeholder='sample@email.com'
+									required
+								/>
+							</div>
+							<div className='form-group mt-3'>
+								<button className='btn btn-primary' type='submit'>
+									Send Reset Link
+								</button>
+							</div>
+						</form>
+
+						{message && <p>{message}</p>}
+					</div>
 				</div>
-				<button type='submit'>Send Reset Link</button>
-			</form>
-			{message && <p>{message}</p>}
+			</div>
 		</div>
 	);
 };
