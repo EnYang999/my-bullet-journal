eea68a4137f5cc8681bc013ee345fe9e94ba650c
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-05-30 13:45:12 -0400

    using cookies fetch token and post sucessfully

--- client/src/components/todolines/todoline.tsx
+++ client/src/components/todolines/todoline.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Cookies from "universal-cookie";
+import { jwtDecode } from "jwt-decode";
 import { toast } from "../errortoast/ErrorToastManager";
 import {
 	API_ENDPOINT,
 	APP_BACKEND_PORT,
-	APP_USER_API,
-	APP_LOGIN_URL,
-	APP_SIGNUP_URL,
-	APP_RESET_PASSWORD,
+	APP_TODO_POST,
+	APP_TODO_API,
+	APP_AUTHENTICATE_TOKEN_NAME,
 } from "../../../../common/constants";
 interface Props {
 	className: string;
@@ -20,24 +21,35 @@ export const ToDoLine = ({
 }: Props): JSX.Element => {
 	const [inputValue, setInputValue] = useState<string>("");
 	const [isChecked, setIsChecked] = useState<boolean>(false);
+	const cookies = new Cookies();
+	const bearToken = cookies.get(APP_AUTHENTICATE_TOKEN_NAME);
+
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setInputValue(event.target.value);
 	};
 	const handleCheckboxChange = () => {
 		setIsChecked(!isChecked);
 	};
-	const handleCheckbox = async (e: React.SyntheticEvent) => {
-		e.preventDefault();
-		const response = await axios.post(
-			`${API_ENDPOINT}${APP_BACKEND_PORT}${APP_USER_API}${APP_LOGIN_URL}`,
-			{
-				completed: isChecked,
-			},
-			{ withCredentials: true }
-		);
-		console.log(response);
-
+	const separatedComponents = boxId.split("-");
+	const todoMonth = separatedComponents[0];
+	const todoWeek = separatedComponents[2];
+	const todoDay = separatedComponents[3];
+	const todoNum = separatedComponents[4];
+	const handlePost = async () => {
 		try {
+			const response = await axios.post(
+				`${API_ENDPOINT}${APP_BACKEND_PORT}${APP_TODO_API}${APP_TODO_POST}/${todoMonth}/${todoWeek}/${todoDay}/${todoNum}`,
+				{
+					completed: isChecked,
+					description: inputValue,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${bearToken}`,
+					},
+				}
+			);
+			console.log(response);
 		} catch (error: any) {
 			console.log("error from client", error);
 
@@ -50,6 +62,37 @@ export const ToDoLine = ({
 			}
 		}
 	};
+	const handleTodoPost = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+		e.preventDefault();
+
+		if (e.key === "Enter") {
+			try {
+				const response = await axios.post(
+					`${API_ENDPOINT}${APP_BACKEND_PORT}${APP_TODO_API}${APP_TODO_POST}/${todoMonth}/${todoWeek}/${todoDay}/${todoNum}`,
+					{
+						completed: isChecked,
+						description: inputValue,
+					},
+					{
+						headers: {
+							Authorization: `Bearer ${bearToken}`,
+						},
+					}
+				);
+				console.log(response);
+			} catch (error: any) {
+				console.log("error from client", error);
+
+				if (error.response) {
+					toast.show({
+						title: "Error",
+						content: error.response.data.error,
+						duration: 3000,
+					});
+				}
+			}
+		}
+	};
 	return (
 		<div className={`todo-wrapper ${className}`}>
 			<input
@@ -59,7 +102,6 @@ export const ToDoLine = ({
 				checked={isChecked}
 				onChange={() => {
 					handleCheckboxChange;
-					handleCheckbox;
 				}}
 				style={{ display: "none" }}
 			/>
@@ -73,6 +115,7 @@ export const ToDoLine = ({
 				<span className={`input-box`}>
 					<input
 						value={inputValue}
+						onKeyUp={handleTodoPost}
 						onChange={handleInputChange}
 						className={`input-field ${isChecked ? "checked" : ""}`}
 						disabled={isChecked}
