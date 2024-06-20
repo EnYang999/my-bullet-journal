3ee6271f5bacc17ca6d5bb3aee0b2c819d6b72c3
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-06-19 20:53:03 -0400

    update http to https

--- common/constants.js
+++ common/constants.js
@@ -1,5 +1,7 @@
 export const API_ENDPOINT =
-	"http://www.enbujo.com:" || "http://enbujo.com:" || "http://147.182.217.227:";
+	"https://www.enbujo.com:" ||
+	"https://enbujo.com:" ||
+	"https://147.182.217.227:";
 // "http://147.182.217.227:";
 export const APP_NAME = "MyBulletJournal";
 export const APP_BACKEND_PORT = 8000;
