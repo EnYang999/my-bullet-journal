4e4b0d862aba865c53423502a805c929bca0ee18
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-06-23 17:36:08 -0400

    suggestions page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import PermissionDeniedPage from "./components/ErrorPage/PermissionPage";
 // import ControlledCheckboxInput from "./components/todolines/todoComp";
 import SymptomForm from "./components/medical/SymptomForm";
 import WelcomePage from "./components/medical/WelcomePage";
+import SuggestionPage from "./components/medical/SuggestionPage";
 function App() {
 	return (
 		<Routes>
@@ -34,7 +35,7 @@ function App() {
 				element={<ResetPasswordNow />}
 			/>
 			<Route path='/month-planner' element={<SymptomForm />} />
-			<Route path='/habits' element={<WelcomePage />} />
+			<Route path='/habits' element={<SuggestionPage />} />
 		</Routes>
 	);
 }
