8874996162af04f56911ecd6db5b08acd671e66e
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-06-23 17:02:34 -0400

    so far so good

--- client/src/components/medical/SymptomForm.tsx
+++ client/src/components/medical/SymptomForm.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import symptomsData from "./symptoms.json";
 import { motion } from "framer-motion";
-import "./SymptomForm.css";
+import chroma from "chroma-js";
+import "./SymptomForm.scss";
 
 // Type definitions based on JSON structure
 interface SymptomData {
@@ -12,6 +13,9 @@ interface SymptomData {
 
 const symptomData: SymptomData = symptomsData;
 
+// Generate a color scale using chroma-js
+const colorScale = chroma.scale(["#fafa6e", "#2A4858"]).mode("lch").colors(50);
+
 const SymptomForm: React.FC = () => {
 	const [selectedCategories, setSelectedCategories] = useState<Set<string>>(
 		new Set()
@@ -19,74 +23,120 @@ const SymptomForm: React.FC = () => {
 	const [selectedSymptoms, setSelectedSymptoms] = useState<Set<string>>(
 		new Set()
 	);
+	const [clickedDetails, setClickedDetails] = useState<Set<string>>(new Set());
+	const [colorMap, setColorMap] = useState<{ [key: string]: string }>({});
+
+	useEffect(() => {
+		// Initialize color map
+		const initialColorMap: { [key: string]: string } = {};
+		let colorIndex = 0;
+
+		// Assign colors to categories and symptoms
+		Object.keys(symptomData).forEach((category) => {
+			initialColorMap[category] = colorScale[colorIndex % colorScale.length];
+			colorIndex++;
+
+			Object.keys(symptomData[category]).forEach((symptom) => {
+				const key = `${category}-${symptom}`;
+				initialColorMap[key] = colorScale[colorIndex % colorScale.length];
+				colorIndex++;
+			});
+		});
+
+		setColorMap(initialColorMap);
+	}, []);
 
 	const handleCategoryClick = (category: string) => {
 		setSelectedCategories((prevState) => {
 			const newSet = new Set(prevState);
-			if (newSet.has(category)) {
-				newSet.delete(category);
-			} else {
-				newSet.add(category);
-			}
+			if (newSet.has(category)) newSet.delete(category);
+			else newSet.add(category);
+
 			return newSet;
 		});
 	};
 
 	const handleSymptomClick = (category: string, symptom: string) => {
+		const key = `${category}-${symptom}`;
 		setSelectedSymptoms((prevState) => {
 			const newSet = new Set(prevState);
-			const key = `${category}-${symptom}`;
-			if (newSet.has(key)) {
-				newSet.delete(key);
-			} else {
-				newSet.add(key);
-			}
+			if (newSet.has(key)) newSet.delete(key);
+			else newSet.add(key);
+
 			return newSet;
 		});
 	};
 
-	return (
-		<div className='container d-flex'>
-			{Object.keys(symptomData).map((category) => (
-				<motion.div
-					key={`${category}`}
-					onClick={() => handleCategoryClick(category)}
-					className='d-flex'
-					whileHover={{ scale: 1.2 }}
-					whileTap={{ scale: 0.8 }}
-				>
-					{category}
-				</motion.div>
-			))}
+	const handleDetailClick = (key: string) => {
+		setClickedDetails((prevState) => {
+			const newSet = new Set(prevState);
+			if (newSet.has(key)) newSet.delete(key);
+			else newSet.add(key);
 
-			{Array.from(selectedCategories).map((category) => (
-				<div key={category}>
-					{Object.keys(symptomData[category]).map((symptom) => (
-						<motion.div
-							key={`${category}-${symptom}`}
-							onClick={() => handleSymptomClick(category, symptom)}
-							className='d-flex'
-							whileHover={{ scale: 1.2 }}
-							whileTap={{ scale: 0.8 }}
-						>
-							{symptom}
-						</motion.div>
-					))}
-				</div>
-			))}
-			{Array.from(selectedSymptoms).map((key) => {
-				const [category, symptom] = key.split("-");
-				return symptomData[category][symptom].map((detail, index) => (
+			return newSet;
+		});
+	};
+
+	const getColor = (key: string) => colorMap[key];
+
+	return (
+		<div className='container icons-container '>
+			<div className='d-inline-flex flex-wrap'>
+				{Object.keys(symptomData).map((category) => (
 					<motion.div
-						key={`${key}-${index}`}
-						className='d-flexs'
+						key={`${category}`}
+						onClick={() => handleCategoryClick(category)}
+						className={`icon ${
+							selectedCategories.has(category) ? "selected" : ""
+						}`}
 						whileHover={{ scale: 1.2 }}
 						whileTap={{ scale: 0.8 }}
+						style={{ backgroundColor: getColor(category) }}
 					>
-						{detail}
+						{category}
 					</motion.div>
-				));
-			})}
+				))}
+				{Array.from(selectedCategories).map((category) => (
+					<div key={category}>
+						{Object.keys(symptomData[category]).map((symptom) => {
+							const key = `${category}-${symptom}`;
+							return (
+								<motion.div
+									key={key}
+									onClick={() => handleSymptomClick(category, symptom)}
+									className={`icon ${
+										selectedSymptoms.has(key) ? "selected" : ""
+									}`}
+									whileHover={{ scale: 1.2 }}
+									whileTap={{ scale: 0.8 }}
+									style={{ backgroundColor: getColor(key) }}
+								>
+									{symptom}
+								</motion.div>
+							);
+						})}
+					</div>
+				))}
+				{Array.from(selectedSymptoms).map((key) => {
+					const [category, symptom] = key.split("-");
+					return symptomData[category][symptom].map((detail, index) => (
+						<motion.div
+							key={`${key}-${index}`}
+							onClick={() => handleDetailClick(`${key}-${index}`)}
+							className={`icon symptom-detail ${
+								clickedDetails.has(`${key}-${index}`) ? "clicked" : ""
+							}`}
+							whileHover={{ scale: 1.2 }}
+							whileTap={{ scale: 0.8 }}
+							style={{
+								backgroundColor: getColor(key),
+							}}
+						>
+							{detail}
+						</motion.div>
+					));
+				})}
+			</div>
 		</div>
 	);
 };
