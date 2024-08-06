070921718af1db88a090ccbdcf7a3aaef1aedc86
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2024-08-05 20:57:37 -0400

    January todoline box

--- client/src/components/January/weekday3.tsx
+++ client/src/components/January/weekday3.tsx
@@ -9,10 +9,10 @@ const WeekBox = ({ day, boxIds }: Props) => {
 			{/* <figure className={`week-fig my-1 w-75`}>
 				<img className={`week-shape`} src={shapeImage} />
 			</figure> */}
-			<div style={{ fontFamily: "cursive" }}>{day}</div>
+			<div style={{ fontFamily: "cursive", fontSize: "2rem" }}>{day}</div>
 
 			<div
-				className={`to-do-lines-container mx-3 flex-grow-1 d-flex flex-column h-80 overflow-auto`}
+				className={`to-do-lines-container mx-3 flex-grow-1 d-flex flex-column h-80`}
 			>
 				{boxIds.map((boxId, index) => (
 					<ToDoLine key={index} className='my-todo-list' boxId={boxId} />
