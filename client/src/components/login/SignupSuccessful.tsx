cc9ef573c76ef94a8c2668c4cb551c6d911300e6
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2026-06-24 13:05:33 -0400

    delete comments

--- client/src/components/login/SignupSuccessful.tsx
+++ client/src/components/login/SignupSuccessful.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./RedirectPage.css"; // Import your CSS file
+import "./RedirectPage.css";
 
 const SignupSuccessful: React.FC = () => {
 	const navigate = useNavigate();
